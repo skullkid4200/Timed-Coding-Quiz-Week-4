9c84610e341c42667665e1d2d6f26113bebe5858
skullkid4200 <jackeinhorn123@gmail.com>
2022-12-18 18:44:44 -0500

    committing and pushing all work so far. button and timer finally work...

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+var startBtn = document.getElementById("startbtn");
+
+
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+hey = function () {
+    var oneMinute = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+};
+
+
+startBtn.addEventListener("click", hey);
