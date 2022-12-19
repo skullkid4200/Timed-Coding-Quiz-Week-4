62b640226d260e62f4a343314e74d514265ee1d9
skullkid4200 <jackeinhorn123@gmail.com>
2022-12-19 16:18:47 -0500

    big push, lots of changings, functional start button, timer works, page changes to next questions, etc.still a lot to add

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 var startBtn = document.getElementById("startbtn");
-
-
+var questions = document.getElementById("question1-screen");
+var quizIntro = document.getElementById("quiz-intro")
+var hideQuestions = true
+var currentQuestion = 0
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -19,11 +21,43 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-hey = function () {
+function hey() {
     var oneMinute = 60 * 1,
         display = document.querySelector('#time');
     startTimer(oneMinute, display);
-};
+    console.log("hey pee pee head");
 
 
-startBtn.addEventListener("click", hey);
+}
+
+function showQuestions() {
+    questions.style.display = "block";
+    questions.style.visibility = "visible";
+    quizIntro.style.display = "none";
+    quizIntro.style.visibility = "hidden";
+    currentQuestion++
+
+}
+
+function wrongAnswer() {
+    //1. subtract time
+    //2. store value
+    //3. go to next question
+}
+
+function rightanswer() {
+    //1. store value
+    //2. go to next question
+}
+
+function showLastPage() {
+if (currentQuestion >= 5){
+    //1. hide current page
+    //2. display last page
+    //3. display stored value
+}
+ 
+}
+
+startBtn.addEventListener("click", hey)
+startBtn.addEventListener("click", showQuestions)
