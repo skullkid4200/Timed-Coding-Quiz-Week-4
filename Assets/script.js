1a5aa348dbaff5f2d37233021acec98fa48271b9
skullkid4200 <jackeinhorn123@gmail.com>
2022-12-19 18:04:32 -0500

    added some more structural code and started the functions to move from page to page.

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,9 @@
 var startBtn = document.getElementById("startbtn");
-var questions = document.getElementById("question1-screen");
+var questions1 = document.getElementById("question1-screen");
+var questions2 = document.getElementById("question2-screen");
+var questions3 = document.getElementById("question3-screen");
+var questions4 = document.getElementById("question4-screen");
+var questions5 = document.getElementById("question5-screen");
 var quizIntro = document.getElementById("quiz-intro")
 var hideQuestions = true
 var currentQuestion = 0
@@ -25,39 +29,82 @@ function hey() {
     var oneMinute = 60 * 1,
         display = document.querySelector('#time');
     startTimer(oneMinute, display);
-    console.log("hey pee pee head");
+
 
 
 }
 
-function showQuestions() {
-    questions.style.display = "block";
-    questions.style.visibility = "visible";
+//FUNCTIONS TO MOVE TO NEXT PAGE
+
+function showQuestions1() {
+    questions1.style.display = "block";
+    questions1.style.visibility = "visible";
     quizIntro.style.display = "none";
     quizIntro.style.visibility = "hidden";
     currentQuestion++
+    console.log(currentQuestion);
+}
+
+function rightAnswer() {
+    currentQuestion++
+    //1. store value
+    //2. go to next question
+    console.log(currentQuestion);
+    if (currentQuestion = 1) {
+        questions2.style.display = "block";
+        questions2.style.visibility = "visible";
+        questions1.style.display = "none";
+        questions1.style.visibility = "hidden";
+        currentQuestion++
+    }
+
+    else if (currentQuestion = 2) {
+        console.log("coolbeans");
+        questions3.style.display = "block";
+        questions3.style.visibility = "visible";
+        questions2.style.display = "none";
+        questions2.style.visibility = "hidden";
+        currentQuestion++
+    }
+
+    else if (currentQuestion = 3) {
+        questions4.style.display = "block";
+        questions4.style.visibility = "visible";
+        questions3.style.display = "none";
+        questions3.style.visibility = "hidden";
+        currentQuestion++
+    }
 
 }
+
 
 function wrongAnswer() {
     //1. subtract time
-    //2. store value
+    //2. add -1 to score
     //3. go to next question
+    if (currentQuestion = 1) {
+        questions2.style.display = "block";
+        questions2.style.visibility = "visible";
+        questions1.style.display = "none";
+        questions1.style.visibility = "hidden";
+        currentQuestion++
+    }
+
 }
 
-function rightanswer() {
-    //1. store value
-    //2. go to next question
-}
+
 
 function showLastPage() {
-if (currentQuestion >= 5){
-    //1. hide current page
-    //2. display last page
-    //3. display stored value
-}
- 
+    if (currentQuestion >= 5) {
+        //1. hide current page
+        //2. display last page
+        //3. display stored value
+    }
+
 }
 
+
+//BUTTON EVENTS
+
 startBtn.addEventListener("click", hey)
-startBtn.addEventListener("click", showQuestions)
+startBtn.addEventListener("click", showQuestions1)
